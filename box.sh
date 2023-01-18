7b2d8db49ad364f22e145124de59bc4382d52daf
Tim0n3 <>
2023-01-18 19:52:40 +0200

    long if else distro check for rhel and debian whiptail and dialog

--- box.sh
+++ box.sh
@@ -84,26 +84,52 @@ function delete_user {
     echo "User $user_to_delete deleted."
 }
 
-# NCurses menu to display options
-while true; do
-    choice=$(whiptail --title "System Setup Menu" --menu "Select an option:" 15 60 7 \
-    "1" "Install system security updates" \
-    "2" "Install Plex Media Server" \
-    "3" "Install Transmission" \
-    "4" "Add new user and chroot-jail" \
-    "5" "Install Netdata" \
-    "6" "Delete user" \
-    "7" "Exit"  3>&1 1>&2 2>&3)
+function check_distribution {
+    distro=$(uname -a)
+    if [[ "$distro" == *"Debian"* ]]; then
+        # NCurses menu to display options
+        while true; do
+            choice=$(whiptail --title "System Setup Menu" --menu "Select an option:" 15 60 7 \
+            "1" "Install system security updates" \
+            "2" "Install Plex Media Server" \
+            "3" "Install Transmission" \
+            "4" "Add new user and chroot-jail" \
+            "5" "Install Netdata" \
+            "6" "Delete user" \
+            "7" "Exit"  3>&1 1>&2 2>&3)
 
+            case $choice in
+                1) update_system;;
+                2) install_plex;;
+                3) install_transmission;;
+                4) add_user;;
+                5) install_netdata;;
+                6) delete_user;;
+                7) exit;;
+                *) echo "Invalid option. Please try again.";;
+            esac
+        done
+    elif [[ "$distro" == *"Red Hat"* ]]; then
+        # NCurses menu to display options
+        while true; do
+            choice=$(dialog --title "System Setup Menu" --menu "Select an option:" 15 60 7 \
+            "1" "Install system security updates" \
+            "2" "Install Plex Media Server" \
+            "3" "Install Transmission" \
+            "4" "Add new user and chroot-jail" \
+            "5" "Install Netdata" \
+            "6" "Delete user" \
+            "7" "Exit"  3>&1 1>&2 2>&3)
 
-    case $choice in
-        1) update_system;;
-        2) install_plex;;
-        3) install_transmission;;
-        4) add_user;;
-        5) install_netdata;;
-        6) delete_user;;
-        7) exit;;
-        *) echo "Invalid option. Please try again.";;
-    esac
-done
+            case $choice in
+                1) update_system;;
+                2) install_plex;;
+                3) install_transmission;;
+                4) add_user;;
+                5) install_netdata;;
+                6) delete_user;;
+                7) exit;;
+                *) echo "Invalid option. Please try again.";;
+            esac
+        done
+    fi
